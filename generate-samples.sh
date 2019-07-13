624c77177e5030e1f956f87d921c812c88ff5990
djfdyuruiry <djfdyuruiry>
2019-07-13 20:25:37 +0100

    Using samples script as test

--- generate-samples.sh
+++ generate-samples.sh
@@ -1,3 +1,6 @@
+#! /usr/bin/env sh
+set -e
+
 ./bin/pretty-md-pdf.js -i README.md -t pdf -o sample/README.pdf
 ./bin/pretty-md-pdf.js -i README.md -t jpeg -o sample/README.jpeg
 ./bin/pretty-md-pdf.js -i README.md -t png -o sample/README.png
