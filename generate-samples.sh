5371a60de1639faa151f0a34e6593234baab4e0d
matthew snoddy <djfdyuruiry@users.noreply.github.com>
2020-01-30 20:23:58 +0000

    v1.2.1
    Bumping Dependencies

--- generate-samples.sh
+++ generate-samples.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env sh
 set -e
 
+rm sample/*
+
 ./bin/pretty-md-pdf.js -i README.md -t pdf -o sample/README.pdf
 ./bin/pretty-md-pdf.js -i README.md -t jpeg -o sample/README.jpeg
 ./bin/pretty-md-pdf.js -i README.md -t png -o sample/README.png
